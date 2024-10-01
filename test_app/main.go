25119624541244613693b0b756508fb1c1f5c590
ZoZo0571 <117384442+ZoZo0571@users.noreply.github.com>
2024-10-02 00:10:04 +0300

    задача 1.12 последняя

--- test_app/main.go
+++ test_app/main.go
@@ -1,26 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
 	var x int
 	fmt.Scan(&x)
+	var y []int
+	if x >= 1 && x <= 100 {
+		y = make([]int, x)
+	} else {
+		fmt.Println("Ошибка: подано число не от 1 до 100")
+		os.Exit(1)
+	}
 
-	y := make([]int, x)
 	for i := 0; i < x; i++ {
 		var a int
 		fmt.Scan(&a)
 		y[i] = a
 
 	}
-
-	for i := range y {
-		if i%2 == 0 {
-			fmt.Print(y[i], " ")
+	summ := 0
+	for _, v := range y {
+		if v > 0 {
+			summ++
 		}
 	}
 
-	fmt.Print("Еба")
+	fmt.Print(summ)
 
 }
