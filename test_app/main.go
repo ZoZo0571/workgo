19c1a772951a9a54a961a76658509f94607aff43
ZoZo0571 <117384442+ZoZo0571@users.noreply.github.com>
2024-10-21 23:42:32 +0300

    Обработка ошибок

--- test_app/main.go
+++ test_app/main.go
@@ -1,5 +1,17 @@
 package main
 
 func main() {
-	task_2_5_6()
+	task_2_6_1()
 }
+
+// package main
+
+// import (
+// 	"errors"
+// 	"fmt"
+// )
+
+// func main() {
+// 	err := errors.New("my error")
+// 	fmt.Println("", err)
+// }
